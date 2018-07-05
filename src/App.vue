3f77730cd96e3945125b11e55a65a9c52b9c43a5
bergpb <lindembergdepaulo@gmail.com>
2018-07-04 21:20:58 -0300

    projeto inicial

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+     <section class="hero is-dark is-bold is-fullheight">
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                Todo List - VueJS / Django
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+      
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <todo-card></todo-card>
+        </div>
+      </div>
+
+      <div class="hero-foot">
+        <div class="container">
+          <div class="content has-text-centered">
+            <!-- aqui vai o conteudo do footer -->
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import TodoCard from './components/TodoCard'
+
+export default {
+  name: 'app',
+  components:{
+    TodoCard
+  },
+  data () {
+    return {
+      msg: 'Hello, World!'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
